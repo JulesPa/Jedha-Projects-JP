4f05fafa1831e475d239f1ba095c374bf2461093
Jules P. <jul.pap.ann@gmail.com>
2024-11-18 01:11:25 +0100

    Update test.py

--- GetAroundProject/API_app/test.py
+++ GetAroundProject/API_app/test.py
@@ -2,14 +2,14 @@ import requests
 
 url = "https://getaround-api-app-82984add3f24.herokuapp.com/predict"
 payload = {
-    "input": [["Citroën", 140411, 100, "diesel", "black", "convertible", True, True, False, False, True, True, True]]
+    "input": [["Citroën", 140411, 100, "diesel", "black", "convertible", true, true, false, false, true, true, true]]
 }
 response = requests.post(url, json=payload)
 
-print("Status Code:", response.status_code)  # Print the status code
-print("Response Text:", response.text)       # Print the full response text
+print("Status Code:", response.status_code)  
+print("Response Text:", response.text)       
 
-# If the status code is 200, parse the JSON
+
 if response.status_code == 200:
     print(response.json())
 else:
